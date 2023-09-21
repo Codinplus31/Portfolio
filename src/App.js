a229e715dce48a18c4801bd60742054e772606a7
Codinplus31 <127848046+Codinplus31@users.noreply.github.com>
2023-09-21 15:25:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg';
-import Sidebar from './sidebar.jsx'
-import Main from './main.jsx'
+import Sidebar from './sidebar.js'
+import Main from './main.js'
 import './Style.css'
 import './App.css';
 
