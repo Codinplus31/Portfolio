e20bc978572bba74581337fb17203577ed51e496
Codinplus31 <127848046+Codinplus31@users.noreply.github.com>
2023-09-22 19:46:36 +0100

    Update header.js

--- src/header.js
+++ src/header.js
@@ -1,15 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Header(){
+    let [size, setsize] = useState(false)
+    let [display, setdisplay] = useState(false)
+// let media = media.
+function query(){
+    let match = window.matchMedia('(max-width: 600px)');
+    if(match.matches){
 
+setsize(true)
+
+
+}else{
+    
+setsize(false)
+
+
+
+
+}
+}
+useEffect(()=>{
+query()
+
+});
+window.addEventListener('resize',query)
+window.addEventListener('change',query)
 
 return (
-    <div className="header">
+    <>
+    <div className="header" style={{position:'fixed'}}>
                 <span>codinplus</span>
                 <div>
-                <img className="header__account--profile__icon" src="svg/profile.svg" alt="icon" />
+                    {size === false?(
+                        <>
+                    <a href="#home">Home</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#project">Project</a>
+                    <a href="#about">About</a>
+                    <a href="#contact">Contact</a>
+                        </>
+                        ):(
+                <img className="menu" onClick={()=>{
+                    setdisplay(false)
+                }} style={{display: 'block', marginRight: '1rem'}} src="svg/list.svg" alt="icon" />
+                )}
                  </div>            
             </div>
+            {size === true?(
+            <div className="mobileheader" style={{display: display === false?'grid': 'none' }}>
+                    <a href="#skills" onClick={()=>{
+                        setdisplay(true)
+                    }}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
+  <path d="M0 1.5A1.5 1.5 0 0 1 1.5 0h13A1.5 1.5 0 0 1 16 1.5v2A1.5 1.5 0 0 1 14.5 5h-13A1.5 1.5 0 0 1 0 3.5v-2zM1.5 1a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5h-13z"/>
+  <path d="M2 2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm10.823.323-.396-.396A.25.25 0 0 1 12.604 2h.792a.25.25 0 0 1 .177.427l-.396.396a.25.25 0 0 1-.354 0zM0 8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8zm1 3v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2H1zm14-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2h14zM2 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"/>
+</svg> <span>Skills</span></a>
+                    <a href="#project" onClick={()=>{
+                        setdisplay(true)
+                    }}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"  viewBox="0 0 16 16">
+  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+  <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+</svg> <span>Project</span></a>
+                    <a href="#about" onClick={()=>{
+                        setdisplay(true)
+                    }}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"  viewBox="0 0 16 16">
+  <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11z"/>
+</svg> <span>About</span></a>
+                    <a href="#contact" onClick={()=>{
+                        setdisplay(true)
+                    }}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
+  <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
+</svg> <span>Contact</span></a>
+
+    <img onClick={()=>{
+                        setdisplay(true)
+                    }} style={{position: 'absolute', top:'1.4%', right: '2.2%'}} src="./x.svg" />
+            </div>):''
+            }
+            </>
 );
 }
 export default Header;
