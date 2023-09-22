94624572de292bb331eabfa9ace18cabaa30bbd5
Codinplus31 <127848046+Codinplus31@users.noreply.github.com>
2023-09-22 19:45:47 +0100

    Update about.js

--- src/about.js
+++ src/about.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 
 function About(){
-
+    let img = useRef();
+    let content = useRef();
+    useEffect(()=>{
+      function AboutTran(elem, classes){
+  
+        const observer = new IntersectionObserver((entry)=>{
+        console.log(entry)
+    entry.forEach(e=>{
+      if(e.isIntersecting){
+        e.target.classList.add(classes)
+        
+      }else{
+        e.target.classList.remove(classes)
+      }
+    })
+  })
+  const Skills = elem;
+  observer.observe(Skills);
+  }
+  AboutTran(img.current, 'aboutimg')
+  AboutTran(content.current, 'aboutcont')
+  },[])
     return (
-        <div className='about'>
-                <img src='isme.jpg' />
-            <div>
+        <div id='about' className='about'>
+                <img ref={img} src='isme.jpg' />
+            <div ref={content}>
             <h1>About Me</h1>
             <p>I am a skilled front-end and back-end developer specializing in creating user-friendly and visually appealing websites. With a strong command of HTML, CSS, and JavaScript and libraries like bootstrap, tailwind css, jQuery, Vuejs, Babylonjs, reactjs.</p>
             <button>Hire Me</button>
