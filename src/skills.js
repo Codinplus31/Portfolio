114b8a036ced9c985c8d501b479a4d576ff8eea5
Codinplus31 <127848046+Codinplus31@users.noreply.github.com>
2023-09-23 09:14:01 +0100

    Update skills.js

--- src/skills.js
+++ src/skills.js
@@ -2,31 +2,38 @@ import React, { useEffect, useRef } from "react";
 
 function Skills(){
   let skills = useRef();
+  let frontEnd = useRef();
+  let backEnd = useRef();
   useEffect(()=>{
+    function ScrollTran(elem, classes){
 
-    const observer = new IntersectionObserver((entry)=>{
+      const observer = new IntersectionObserver((entry)=>{
       console.log(entry)
   entry.forEach(e=>{
     if(e.isIntersecting){
-      e.target.classList.add('skills-show')
+      e.target.classList.add(classes)
       
     }else{
-      e.target.classList.remove('skills-show')
+      e.target.classList.remove(classes)
     }
   })
 })
-const Skills = skills.current;
+const Skills = elem;
 observer.observe(Skills);
+}
+ScrollTran(skills.current, 'skills-show')
+ScrollTran(frontEnd.current, 'frontend')
+ScrollTran(backEnd.current, 'backend')
 },[])
 
     return(
-        <div ref={skills} className="Skills">
+        <div ref={skills} id="skills" className="Skills">
                   <h1><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
   <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
 </svg>   Skills    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
   <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
 </svg></h1>
-                  <div className="cat">
+                  <div className="cat" ref={frontEnd}>
                     <h3>Frond-end</h3>
                     <div title="javascript"><img src='javascript.svg' width='50%' height='50%'/></div>
                     <div title="reactjs"><img src='react.svg' width='50%' height='50%'/></div>
@@ -37,13 +44,14 @@ observer.observe(Skills);
                     <div title="css"><img src='css.svg' width='50%' height='50%'/></div>
                     <div title="jQuery"><img src='jquery.svg' width='50%' height='50%'/></div>
                   </div>
-                  <div className="cat">
+                  <div className="cat" ref={backEnd}>
                     <h3>Back-end</h3>
                     <div title="nodejs"><img src='node.svg' width='50%' height='50%'/></div>
                     <div title="expressjs"><img src='express.svg' width='50%' height='50%'/></div>
                     <div title="socket.io"><img src='socket.svg' width='50%' height='50%'/></div>
                     <div title="mysql"><img src='mysql.svg' width='60%' height='60%'/></div>
                     <div title="postgresql"><img src='postgresql.svg' width='50%' height='50%'/></div>
+                    <div title="php"><img src='php.svg' width='50%' height='50%'/></div>
                 
                   </div>
         </div>
