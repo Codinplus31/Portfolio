5e161a1cf0c12a723d7c179107296a1731a827d2
Codinplus31 <127848046+Codinplus31@users.noreply.github.com>
2023-09-26 19:34:44 +0100

    Update project.js

--- src/project.js
+++ src/project.js
@@ -2,7 +2,7 @@ import React, {useEffect, useRef} from "react";
 function Project(){
   let project1 = useRef();
   let project2 = useRef();
-  let project3 = useRef();
+ // let project3 = useRef();
   useEffect(()=>{
     function ProjectTran(elem, classes){
 
@@ -22,7 +22,7 @@ observer.observe(Skills);
 }
 ProjectTran(project1.current, 'projtran')
 ProjectTran(project2.current, 'projtran')
-ProjectTran(project3.current, 'projtran')
+//ProjectTran(project3.current, 'projtran')
 },[])
                
     return (
@@ -63,23 +63,7 @@ ProjectTran(project3.current, 'projtran')
 </svg></a>
                     </div>
                 </div>
-                <div ref={project3} className="projsec">
-                  <div className="img">
-                    <img src="image/3dcandy.jpg"/>
-
-                  </div>
-                    <h2>Project Name</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia maiores, quia dolorem obcaecati distinctio magnam cupiditate sit voluptas, dolor placeat, fugiat asperiores possimus qui at quasi eveniet nesciunt laborum atque.</p>
-                    <div>
-                        <a><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
-  <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z"/>
-</svg></a>
-                        <a><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
-  <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
-  <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
-</svg></a>
-                    </div>
-                </div>
+                
               
 
               </div>
